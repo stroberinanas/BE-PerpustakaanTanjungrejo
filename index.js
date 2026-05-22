b47e9494ead6f9f04bde80e938eecb50bb58bbd4
stroberinanas <sabilakhairina2@gmail.com>
2026-05-22 19:00:18 +0700

    static url

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ import { verifyAdminSession } from "./middlewares/authAdmin.js";
 dotenv.config();
 app.use(express.json());
 app.use(cors());
-app.use(express.static("public"));
 
+app.use(express.static(path.join(__dirname, "../fe-perpustanjungrejo")));
 
 app.get("/", (req, res) => {
     res.sendFile(
